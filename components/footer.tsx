c6ec34b607237193c34438943d640b19866dc721
rmedina13118 <echemedirichm@gmail.com>
2026-02-14 02:06:24 +0000

    feat: add padding to footer logo section for better spacing

--- components/footer.tsx
+++ components/footer.tsx
@@ -38,7 +38,7 @@ export function Footer() {
             <ul className="space-y-2 text-sm text-[hsl(200,30%,60%)]">
               <li>Álgebra Lineal <br /> 2° Semestre - Módulo 1</li>
               <li>Profesor: Rafael Martinez</li>
-              <li>
+              <li className="pt-2">
                 <img src={'/logo-jala.webp'} width={100} height={100} alt="logo-jala" />
                 <p>Jala University 2025</p>
               </li>
